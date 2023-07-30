bcde1efc82b7043973ca93b51a10ec62a0cf2146
luha987 <69619849+luha987@users.noreply.github.com>
2023-07-30 23:23:06 +0700

    Add files via upload

---
+++ lib/providers/all_products.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import './product.dart';
+import 'dart:math';
+
+class Products with ChangeNotifier {
+  List<Product> _allproducts = List.generate(
+    25,
+    (index) {
+      return Product(
+        id: "id_${index + 1}",
+        title: "Product ${index + 1}",
+        description: 'Ini adalah deskripsi produk ${index + 1}',
+        price: 10 + Random().nextInt(100).toDouble(),
+        imageUrl: 'https://picsum.photos/id/$index/200/300',
+      );
+    },
+  );
+
+  List<Product> get allproducts {
+    return _allproducts;
+  }
+
+  Product findById(productId) {
+    return _allproducts.firstWhere((prodId) => prodId.id == productId);
+  }
+
+}
